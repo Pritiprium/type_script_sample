e9bdbdbc564d2d87f9b59b37df5db31ad1b893ac
priti prium <pritiprium23@gmail.com>
2020-05-17 13:05:41 -0500

    second commit

--- config.ts
+++ config.ts
@@ -2,4 +2,4 @@ import {Config} from 'protractor'
 export let config : Config={
     seleniumAddress : 'http://localhost:4444/wd/hub',
     specs:['spec.js']
-};
+}; 
--- spec.ts
+++ spec.ts
@@ -1,7 +1,7 @@
 import{browser} from 'protractor'
 describe('Test Suite Sample',function(){
-    it('Test case 1',function(){
+    it('Test case 1',function(){        
         browser.get('http://www.angular.io');
-        browser.sleep(3000)
+        browser.sleep(2000)
     });
 });
